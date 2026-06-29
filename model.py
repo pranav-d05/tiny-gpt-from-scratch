932a3f37d4964a8b872702b1c02019c5de84df02
Pranav Dhebe <pranavdhebe93@gmail.com>
2026-06-29 17:11:29 +0530

    Step 36: pick_split_point

--- model.py
+++ model.py
@@ -305,8 +305,11 @@ def encode_corpus_to_int_array(text, stoi):
     # TODO: map every character in text through stoi and return as a 1D int64 array
     return np.array(encode_string(text,stoi))
 
-# Step 36 - pick_split_point (not yet solved)
-# TODO: implement
+# Step 36 - pick_split_point
+def pick_split_point(n, train_frac):
+    """Return integer split index so data[:idx] is train and data[idx:] is val."""
+    # TODO: compute the integer split index from n and train_frac
+    return int(n*train_frac)
 
 # Step 37 - slice_train_and_val (not yet solved)
 # TODO: implement
