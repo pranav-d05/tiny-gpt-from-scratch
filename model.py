f931b46c1f2a7f97e6153734a413a261d6d8cba7
Pranav Dhebe <pranavdhebe93@gmail.com>
2026-06-29 17:53:46 +0530

    Step 54: log_prob_of_pair

--- model.py
+++ model.py
@@ -471,8 +471,13 @@ def decode_generated_sequence(ids, itos):
     
     return st
 
-# Step 54 - log_prob_of_pair (not yet solved)
-# TODO: implement
+# Step 54 - log_prob_of_pair
+def log_prob_of_pair(p_matrix, current_id, next_id):
+    """Return the log probability of a single (current, next) bigram."""
+    # TODO: pick out P[current_id, next_id] and return its natural log
+    ele = index_element(p_matrix,current_id,next_id)
+
+    return array_log(ele)
 
 # Step 55 - sum_negative_log_probs (not yet solved)
 # TODO: implement
