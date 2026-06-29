d8ac053384b0c3bf8ad340c126ed7ed33090d511
Pranav Dhebe <pranavdhebe93@gmail.com>
2026-06-29 17:28:18 +0530

    Step 44: get_batch

--- model.py
+++ model.py
@@ -375,8 +375,13 @@ def stack_y_batch(data, offsets, block_size):
 
     return np.array(batch)
 
-# Step 44 - get_batch (not yet solved)
-# TODO: implement
+# Step 44 - get_batch
+def get_batch(data, block_size, batch_size, rng):
+    # TODO: package one training batch (X, Y) of shape (batch_size, block_size) from data using rng.
+    offset = sample_random_batch_offsets(len(data),block_size,batch_size,rng)
+    X = stack_x_batch(data,offset,block_size)
+    y = stack_y_batch(data,offset,block_size)
+    return (X,y)
 
 # Step 45 - allocate_count_matrix (not yet solved)
 # TODO: implement
