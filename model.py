dcd6a1f22c63e75c39e6581d9626a64360028b3c
Pranav Dhebe <pranavdhebe93@gmail.com>
2026-06-30 23:19:05 +0530

    Step 89: layernorm_backward_divide_std

--- model.py
+++ model.py
@@ -841,8 +841,11 @@ def layernorm_backward_subtract_mean(dy, cache):
     dx = dy - np.mean(dy, axis=1, keepdims=True)
     return dx
 
-# Step 89 - layernorm_backward_divide_std (not yet solved)
-# TODO: implement
+# Step 89 - layernorm_backward_divide_std
+def layernorm_backward_divide_std(dy, cache):
+    """Propagate dy through the divide-by-std step of LayerNorm."""
+    # TODO: propagate the upstream gradient through the divide-by-std step of LayerNorm
+    return dy / np.sqrt(cache['var']+cache['eps'])
 
 # Step 90 - layernorm_backward_full (not yet solved)
 # TODO: implement
