1c5e6fdc80521eb69781728b7dd486049bb28f69
Pranav Dhebe <pranavdhebe93@gmail.com>
2026-06-30 14:09:10 +0530

    Step 74: linear_forward

--- model.py
+++ model.py
@@ -691,8 +691,14 @@ def sample_from_neural_bigram(w, start_id, num_tokens, itos):
     
     return decode_ids(ans,itos)
 
-# Step 74 - linear_forward (not yet solved)
-# TODO: implement
+# Step 74 - linear_forward
+def linear_forward(x, w):
+    # TODO: compute Y = X @ W and return {'y': Y, 'cache': {'x': x, 'w': w}}.
+    y = x @ w 
+    return {
+        'y':y,
+        'cache':{'x':x,'w':w}
+    }
 
 # Step 75 - derive_dx_on_paper (not yet solved)
 # TODO: implement
