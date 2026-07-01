0c55a7bd60bd62bcdc1914c2868b025a04127817
Pranav Dhebe <pranavdhebe93@gmail.com>
2026-07-01 22:01:02 +0530

    Step 107: softmax_attention_weights

--- model.py
+++ model.py
@@ -1039,8 +1039,16 @@ def apply_causal_mask(scaled_scores, causal_mask):
     # TODO: return a (B,T,T) array where positions with causal_mask False are -inf...
     return np.where(causal_mask, scaled_scores, float('-inf'))
 
-# Step 107 - softmax_attention_weights (not yet solved)
-# TODO: implement
+# Step 107 - softmax_attention_weights
+import numpy as np
+
+def softmax_attention_weights(masked_scores):
+    """Row-wise stable softmax over the last axis of (B, T, T) scores."""
+    # TODO: apply numerically stable softmax along the last axis of masked_scores
+    d = masked_scores.shape[-1]
+    e_x = np.exp(masked_scores - np.max(masked_scores,axis=-1,keepdims=True))
+
+    return e_x / np.sum(e_x,axis=-1,keepdims=True)
 
 # Step 108 - attention_weighted_values (not yet solved)
 # TODO: implement
